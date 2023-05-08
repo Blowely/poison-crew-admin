f26382c9ca10ff625a6a83ac36c71cfa7ba31d63
Blowely <maryashin.2014@yandex.ru>
2023-05-08 13:51:28 +0300

    init

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,42 @@
+import NextAuth, {getServerSession} from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import Yandex from "next-auth/providers/yandex";
+
+const adminEmails = ['maryashin.2014@yandex.ru'];
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+    Yandex({
+      clientId: process.env.YANDEX_ID,
+      clientSecret: process.env.YANDEX_SECRET
+    }),
+  ],
+
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({session,token,user}) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
+  }
+}
