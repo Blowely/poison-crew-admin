3ca60f579aba12619ec84e75a6302fa1c82f56d3
Blowely <maryashin.2014@yandex.ru>
2023-06-01 02:03:49 +0300

    commented auth

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,9 @@ import { SessionProvider } from "next-auth/react";
 
 export default function App({Component, pageProps: { session, ...pageProps }}) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps}/>
-    </SessionProvider>
+    /*<SessionProvider session={session}>
+
+    </SessionProvider>*/
+    <Component {...pageProps}/>
   )
 }
