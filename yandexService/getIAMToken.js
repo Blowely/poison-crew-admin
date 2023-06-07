2d927ac3c3eea8434bc6dd9b0796d2034e490f6a
Blowely <maryashin.2014@yandex.ru>
2023-06-07 21:48:16 +0300

    fix reinstall iam-token

--- yandexService/getIAMToken.js
+++ yandexService/getIAMToken.js
@@ -4,19 +4,20 @@ const jose = require('node-jose');
 const fs = require('fs');
 const axios = require("axios");
 
-const key = fs.readFileSync(process.cwd() + '/yandexService/privateKeyIMG2TXT.txt', 'utf8');
-
-const serviceAccountId = process.env.YANDEX_IMG2TXT_ISS_ID;
-const keyId = process.env.YANDEX_IMG2TXT_KEY_ID;
-const now = Math.floor(new Date().getTime() / 1000);
-
-const payload = { aud: "https://iam.api.cloud.yandex.net/iam/v1/tokens",
-  iss: serviceAccountId,
-  iat: now,
-  exp: now + 3600 };
-
 
 const getJWT = async () => {
+  const key = fs.readFileSync(process.cwd() + '/yandexService/privateKeyIMG2TXT.txt', 'utf8');
+
+  const serviceAccountId = process.env.YANDEX_IMG2TXT_ISS_ID;
+  const keyId = process.env.YANDEX_IMG2TXT_KEY_ID;
+  const now = Math.floor(new Date().getTime() / 1000);
+
+  const payload = { aud: "https://iam.api.cloud.yandex.net/iam/v1/tokens",
+    iss: serviceAccountId,
+    iat: now,
+    exp: now + 3600 };
+
+
   const res = await jose.JWK.asKey(key, 'pem', { kid: keyId, alg: 'PS256' })
   const signer = await jose.JWS.createSign({ format: 'compact' }, res).update(JSON.stringify(payload))
   return await signer.final();
