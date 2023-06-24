158160ff43c444cb4cd98d0981c6ee93e1e5fd66
Blowely <maryashin.2014@yandex.ru>
2023-06-24 21:23:03 +0300

    bug fix

--- pages/api/accounts.js
+++ pages/api/accounts.js
@@ -23,27 +23,22 @@ export default async function handle(req, res) {
 
   if (method === 'POST') {
     try {
-      const phone = query?.accPhone;
-      const type = query?.type;
+      const {phone} = decryptToken(query?.token);
+      //const type = query?.type;
 
-      if (type === 'address') {
-        const {address} = JSON.parse(req.body);
+      const {address} = JSON.parse(req.body);
 
-        const account = await Client.findOne({phone: phone});
-        const result = await Client.updateOne({phone}, {$set: {addresses: [...account?.addresses, address]}})
+      const account = await Client.findOne({phone: phone});
+      const result = await Client.updateOne({phone}, {$set: {addresses: [...account?.addresses, address]}})
 
-        if (result) {
-          res.json({status: 'ok', message: 'Адрес успешно добавлен'});
-          res.status(200);
-        } else {
-          res.json({status: 'internalServerError', message: 'Ошибка сервера'});
-          res.status(500);
-        }
+      if (result) {
+        res.json({status: 'ok', message: 'Адрес успешно добавлен'});
+        res.status(200);
+      } else {
+        res.json({status: 'internalServerError', message: 'Ошибка сервера'});
+        res.status(500);
       }
 
-      if (type === 'cart') {
-
-      }
 
     } catch (e) {
       res.json({status: 'internalServerError', message: 'Ошибка сервера'});
