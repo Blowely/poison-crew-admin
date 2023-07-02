db9d33dff7cd78cd9c4ea47b01db30a1a8664668
Blowely <maryashin.2014@yandex.ru>
2023-07-02 13:41:25 +0300

    clean code

--- pages/api/products.js
+++ pages/api/products.js
@@ -37,7 +37,7 @@ export default async function handle(req, res) {
           if (search) {
             obj.title = new RegExp('.*' + search + '.*');
           }
-          console.log('obj',obj);
+
           return obj;
         }
 
