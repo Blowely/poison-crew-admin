01d596047e6f6a707bbe7b052eac57435e2e91b3
Blowely <maryashin.2014@yandex.ru>
2023-07-06 00:27:50 +0300

    added BB fields

--- models/Address.js
+++ models/Address.js
@@ -1,11 +1,13 @@
 import {model, Schema, models} from "mongoose";
 
 export const AddressSchema = new Schema({
+  id: String,
   fio: {type:String, required:true},
   phone: {type:String, required:true},
   city: {type:String, required:true},
   address: {type:String, required:true},
   postalCode: {type:String, required:true},
+  workschedule: String,
 }, {
   timestamps: true,
 });
