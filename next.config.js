088064d9709f3db67db86de6dca4b72e72e4f592
Blowely <maryashin.2014@yandex.ru>
2023-07-18 22:52:32 +0300

    destination

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
       {
         // matching all API routes
         source: "/api/:path*",
+        destination: 'https://api.re-poizon.ru/:path*',
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
