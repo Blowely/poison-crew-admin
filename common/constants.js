a4d3f31d855fb50af3a94f95c5884c833fedb8e0
Blowely <maryashin.2014@yandex.ru>
2023-07-23 22:22:59 +0300

    orders table changes. added new statuses

--- common/constants.js
+++ common/constants.js
@@ -1,6 +1,8 @@
 export const PRODUCT_STATUS = {
     CREATED: 'created',
     APPROVED: 'approved',
+    APPROVED_WITH_CHANGES: 'approved_with_changes',
     PAYMENT_CHECK: 'payment_check',
     PAID: 'paid',
+    CANCELED: 'canceled',
 }
--- pages/orders.js
+++ pages/orders.js
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import {message, Select} from "antd";
 import {PRODUCT_STATUS} from "@/common/constants";
+import Link from "next/link";
 
 export default function OrdersPage() {
   const [orders,setOrders] = useState([]);
@@ -39,6 +40,7 @@ export default function OrdersPage() {
       <table className="basic">
         <thead>
           <tr>
+            <th>id</th>
             <th>Date</th>
             <th>Paid</th>
             <th>Адрес</th>
@@ -49,6 +51,9 @@ export default function OrdersPage() {
         <tbody>
         {orders.length > 0 && orders.map(order => (
           <tr key={order._id} style={{borderBottom: '1px solid grey'}}>
+            <td>
+              <span style={{fontSize: '10px'}}>{order._id}</span>
+            </td>
             <td>{(new Date(order.createdAt)).toLocaleString()}
             </td>
             <td className={order?.paid ? 'text-green-600' : 'text-red-600'}>
@@ -60,11 +65,21 @@ export default function OrdersPage() {
             <td >
               {order?.products?.map((p) => (
                 <div key={p._id} style={{borderBottom: '1px solid grey'}}>
-                  {p.title}
-                  <br/>
-                  {p.size} размер
-                  <br/>
-                  {p.price} руб
+                  <div>
+                    <img src={p.images[0]} alt=""/>{p.title}
+                    <br/>
+                    {p.size} размер
+                    <br/>
+                    {p.price} руб
+                    <br/>
+                  </div>
+                  <Link className="btn-default" href={'/products/edit/'+p._id}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                    </svg>
+                    Edit
+                  </Link>
+                  <a href={p?.src}>poizon</a>
                   <br/>
                 </div>
               ))}
