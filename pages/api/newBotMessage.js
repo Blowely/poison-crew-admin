20dc3d8c9bc9579b8cfe54692bb9fc5ca1ea8858
Blowely <maryashin.2014@yandex.ru>
2023-07-24 02:42:23 +0300

    test

--- pages/api/newBotMessage.js
+++ pages/api/newBotMessage.js
@@ -5,7 +5,7 @@ export default async function handler(req,res) {
     const {method, query} = req;
     await mongooseConnect();
 
-    const TELEGRAM_URI = `${process.env.TELEGRAM_URI}/sendMessage`
+    const TELEGRAM_URI = `https://api.telegram.org/bot5815209672:AAGETufx2DfZxIdsm1q18GSn_bLpB-2-3Sg/sendMessage`
 
     if (method === 'POST') {
         try {
@@ -19,8 +19,8 @@ export default async function handler(req,res) {
             }
 
             await axios.post(TELEGRAM_URI, {
-                chat_id: process.env.TELEGRAM_CHAT_ID,
-                text: text || 'chatId = ' + chatId
+                chat_id: 664687823,
+                text: text || 'lol'
             })
             res.send('Done')
         } catch (e) {
