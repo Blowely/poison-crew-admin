1b8370375f02ba96e34b42c6c529cb7d72e907e2
Blowely <maryashin.2014@yandex.ru>
2023-07-25 21:46:50 +0300

    fix

--- pages/api/updateStatus.js
+++ pages/api/updateStatus.js
@@ -38,6 +38,7 @@ export default async function handler(req,res) {
                 ---PAYMENT CHECK---\n
                 id: ${orderId}\n
                 ${selectedOrder?.products?.map(el => {
+                        console.log('el =', el);
                         totalPrice += Math.ceil(Number(el?.price) * 13.3 + 1000);
                         return `${el?.title} (${el?.size}) - ${el?.price} CNY;\n
                     ${el?.src[0]}\n
