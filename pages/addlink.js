b6fa6dcbd74de1f22fcbf235ff2594db6ecb791a
Blowely <maryashin.2014@yandex.ru>
2024-03-09 21:41:19 +0300

    fix addlink url

--- pages/addlink.js
+++ pages/addlink.js
@@ -129,7 +129,7 @@ export default function Addlink() {
     const payload = {src:productLink, titleDescription}
 
     if (productLink?.length !== 0) {
-      axios.post("http://localhost:3000/api/productsV2", payload)
+      axios.post("https://api.re-poizon.ru/api/productsV2", payload)
           .then((res) => {
             notification.success({message: `${productLink} добавлен`})
           }).catch((err) => {
