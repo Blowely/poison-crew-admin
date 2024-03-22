0e3e96a08ba607af2943ab66f0f13fac79951454
Blowely <maryashin.2014@yandex.ru>
2024-03-22 13:35:53 +0300

    button

--- pages/links.js
+++ pages/links.js
@@ -149,9 +149,14 @@ export default function Products() {
             {products.items?.map(product => (
               <tr key={product._id} className="flex items-center justify-start gap-2">
                 <td style={{paddingLeft: '0px', paddingRight: 0}} onClick={() => onPaginationChange(Number(lsCurrentPage) + 1, product.src)}>
-                  <a href={`${product.src}`} style={{paddingLeft: 0, opacity: 0}} target="_blank">
+                  <a href={`${product.src}`} style={{paddingLeft: 0}} target="_blank">
                     {getSrcEnding(product.src)}
                   </a>
+                </td>
+                <td style={{paddingLeft: '0px', paddingRight: 0}} onClick={() => onPaginationChange(Number(lsCurrentPage) + 1, product.src)}>
+                  <button onClick={() => {window.open(product.src)}}>
+                    {getSrcEnding(product.src)}
+                  </button>
                 </td>
               </tr>
             ))}
@@ -165,7 +170,6 @@ export default function Products() {
             showQuickJumper
             showTotal={(total) => `Total ${total} items`}
             onChange={onPaginationChange}
-            style={{userSelect: "none"}}
           />
         </>
       }
