8903e78911aa2cff92f72ffb30d8c95d1532dda3
Blowely <maryashin.2014@yandex.ru>
2024-07-25 18:14:42 +0300

    implemented competitor check

--- poizonParseAuth.js
+++ poizonParseAuth.js
@@ -1,7 +1,8 @@
 (async () => {
-  for (let i = 1001482; i <= 1002481; i++) {
+  for (let i = 1002486; i <= 1022486; i++) {
     try {
       const res = await fetch(`https://api.re-poizon.ru/api/productsV3?spuId=${i}&parse=true`)
+      //const res = await fetch(`http://localhost:3001/api/productsV3?spuId=${i}&parse=true`)
       console.log(`i = ${i} res = ${res.ok}`)
     } catch (e) {
       console.log('e =', e?.message)
