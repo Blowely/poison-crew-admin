d21fa87b0a8fb143208bde532420898a6a32d957
Blowely <maryashin.2014@yandex.ru>
2024-07-25 21:04:44 +0300

    1027486

--- poizonParseAuth.js
+++ poizonParseAuth.js
@@ -1,5 +1,5 @@
 (async () => {
-  for (let i = 1005130; i <= 1022486; i++) {
+  for (let i = 1005130; i <= 1027486; i++) {
     try {
       const res = await fetch(`http://localhost:3001/api/simpleProductsV3?spuId=${i}&parse=true`)
       //const res = await fetch(`http://localhost:3001/api/productsV3?spuId=${i}&parse=true`)
