255205ea7ae2f54407f2b7d6d64a42d204ed4e18
Blowely <maryashin.2014@yandex.ru>
2024-07-25 12:47:39 +0300

    1000792

--- poizonParseAuth.js
+++ poizonParseAuth.js
@@ -1,5 +1,5 @@
 (async () => {
-  for (let i = 1000000; i <= 1020000; i++) {
+  for (let i = 1000792; i <= 1020000; i++) {
     try {
       const res = await fetch(`https://api.re-poizon.ru/api/productsV3?spuId=${i}&parse=true`)
       console.log(`i = ${i} res = ${res.ok}`)
