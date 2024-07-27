a65de8e23e217407fe80505ef482a97d283ee0c6
Blowely <maryashin.2014@yandex.ru>
2024-07-27 21:06:55 +0300

    added product options

--- common/utils.js
+++ common/utils.js
@@ -113,6 +113,12 @@ export const handlePoizonProductResponse = (poizonProduct) => {
     const skus = data?.skus || [];
     const salePropertiesList = data?.saleProperties?.list || [];
     const spuId = data?.detail?.spuId || "";
+    const categoryName = data?.detail?.categoryName || "";
+    const categoryId = data?.detail?.categoryId || "";
+    const level1CategoryId = data?.detail?.level1CategoryId || "";
+    const level2CategoryId = data?.detail?.level2CategoryId || "";
+    const brandId = data?.detail?.brandId || "";
+    const commonQuestionRes = data?.detail?.commonTipsModel?.commonQuestionRes?.questionAndAnswerList || [];
     const sizeInfoList = data?.sizeDto?.sizeInfo?.sizeTemplate?.list || [];
 
     const pricesAndSkuIds = []
@@ -149,9 +155,13 @@ export const handlePoizonProductResponse = (poizonProduct) => {
       title,
       cheapestPrice: getCheapestPrice(prices),
       sizesAndPrices,
-      category: '66029ec83d545163a2435edb',
-      brand: 'nike',
+      brandId,
       images,
+      categoryName,
+      categoryId,
+      level1CategoryId,
+      level2CategoryId,
+      commonQuestionRes,
       sizeInfoList,
       isDeleted: false
     };
